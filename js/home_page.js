395bbb91712f336bab8c014e29b31465fba6e7cb
MUSALA\ivan.darpatov <ivan.darpatov@musala.com>
2019-04-02 19:45:16 +0300

    added JS file and imported it in HTML

---
+++ js/home_page.js
@@ -0,0 +1,22 @@
+var laTourDePamTimezone = 1;
+var sofiaTimezone = 2;
+var rejkjavikTimezone = 0;
+var selectedCity = null;
+
+function onBodyLoad() {
+	console.log("my body is ready");
+	
+	var timeContainer = document.getElementById("time_container");
+}
+
+function onSofiaButtonClicked() {
+	console.log("sofia");
+}
+
+function onRejkjavikButtonClicked() {
+	console.log("rejk");
+}
+
+function onLaTourDePamButtonClicked() {
+	console.log("ltdp");
+}
